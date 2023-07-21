435a759e6ad6737672d0dcb7edd1982fb5cc1579
EthanG28 <ethan.garcia.araujo@gmail.com>
2023-07-20 21:57:33 -0400

    Initial commit

---
+++ recetas.js
@@ -0,0 +1,10 @@
+var boton = document.getElementById('nav-menu-link');
+
+function alertaClick() {
+    
+    window.location.href = 'https://www.directoalpaladar.com/directo-al-paladar/las-27-mejores-recetas-de-platos-italianos';
+}
+
+boton.addEventListener("click", alertaClick);
+
+
